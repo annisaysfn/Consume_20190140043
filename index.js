41a2b46f77912fc9f441deca8ae330699d8bb749
annisaysfn <63139331+annisaysfn@users.noreply.github.com>
2022-01-12 00:27:49 +0700

    updatee

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    $("name").text(profile.getName());
-    $("email").text(profile.getEmail());
-    $("image").attr('src', profile.getImageUrl());
-    $(".data").css("display","block");
-    $(".g-signin2").css("display","none");
-  }
+  var profile = googleUser.getBasicProfile();
+  $("#name").text(profile.getName());
+  $("#email").text(profile.getEmail());
+  $(".data").css("display", "block");
+  $(".g-signin2").css("display", "none");
+}
 
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
       alert("You have been signed out successfully");
-      $(".g-signin2").css("display","block");
-      $(".data").css("display","none");
-    });
-  }
+      $(".data").css("display", "none");
+      $(".g-signin2").css("display", "block");
+  });
+}
+
+$("#dataTable").ready(function () {
+  var tabel = document.getElementById("dataTable")
+  getAll().then(response => {
+      console.log(response)
+      for(var i = 0; i <response.length; i++){
+          const tr = tabel.insertRow()
+          const td1 = tr.insertCell();
+          const td2 = tr.insertCell();
+          const td3 = tr.insertCell();
+          const td4 = tr.insertCell();
+          const td5 = tr.insertCell();
+          const td6 = tr.insertCell();
+          console.log(response[i])
+          td1.innerHTML = response[i].nik
+          td2.innerHTML = response[i].nama
+          td3.innerHTML = response[i].jenisKelamin
+          td4.innerHTML = response[i].alamat
+          td5.innerHTML = response[i].divisi
+          td6.innerHTML = `<div class ="justify content-center">
+          <a class="btn ms-2" style="background-color: #858ab3; border:none;" href="edit_data.html?nik=${response[i].nik}">Edit</a>
+          <button type ="button" class="btn ms-2" style="background-color: #858ab3; border:none;" onclick="del(${response[i].nik});">Delete</button>
+          </div>`
+          }
+      }
+  )
+});
